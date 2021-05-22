c19a4687344975135350fe61bf58825ca72eef31
guangzhengh <guangzhengh@student.unimelb.edu.au>
2021-05-22 07:06:11 -0700

    sentiment

--- play.py
+++ play.py
@@ -3,9 +3,5 @@ import json
 from backend.test import *
 
 cdb = CouchDB()
-db = cdb.get_db('melbourne20_21')
-table = db.view('_design/dictionary/_view/date', group=True, group_level=1)
 
-for v in table:
-    print(v.key, v.value)
-
+db = cdb.get_db('sentiment_covid')
